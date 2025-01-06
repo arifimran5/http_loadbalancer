1ed0ef2c288232301c8b9813b0fa5f29a756e5dd
arifimran5 <arifimran505@gmail.com>
2025-01-07 00:08:54 +0530

    new route for test

--- test-server.go
+++ test-server.go
@@ -11,8 +11,13 @@ func TestServer() {
 	// a simple go server with port from command line
 	port := os.Args[1]
 	fmt.Println("Starting server on port", port)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		// time.Sleep(5 * time.Second)
+		log.Println("Received request from", r.RemoteAddr, "on port", port)
 		fmt.Fprintf(w, "Hello, World! from port %s \n", port)
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Healthy %s \n", port)
 	})
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
